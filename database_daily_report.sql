5f3757c49ad96a91ed3f32d0caef59581dd80d9a
tekra <tekrabyte@gmail.com>
2025-10-31 02:07:52 +0000

    a

--- database_daily_report.sql
+++
@@ -1,5 +0,0 @@
--- Update email_config table untuk menambah kolom last_daily_report_sent
--- Untuk tracking kapan terakhir kirim laporan harian
-
-ALTER TABLE `email_config` 
-ADD COLUMN `last_daily_report_sent` DATETIME NULL DEFAULT NULL AFTER `last_sent`;
